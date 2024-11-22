cf26422ae4f3babd8fcf39307d1f94026fe18c72
suchindale <ant92@hotmail.com>
2024-11-22 15:14:39 +0700

    Add files via upload

---
+++ config-server/src/main/java/org/mslib/fis/ConfigServerApplication.java
@@ -0,0 +1,17 @@
+package org.mslib.fis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
+
+
